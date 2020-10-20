740669c821591f5de78ac0388b7c1f0a6f6d9931
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-10-20 19:38:49 -0400

    schema done

--- db/schema.sql
+++ db/schema.sql
@@ -16,5 +16,15 @@ Create Table department (
     title VARCHAR (50) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
-    
-)
+    CONSTRAINT fk_department FOREIGN KEY (department_id) References department(id) on delete cascade
+);
+
+--Another Employee Table--
+Create Table department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR (50) UNIQUE NOT NULL,
+    last_name DECIMAL NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    CONSTRAINT fk_department FOREIGN KEY (manager_id) References employee (id) on delete set null
+);
