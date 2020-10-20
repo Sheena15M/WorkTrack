d64c4998840b494e23c99cf54cb02749246257f2
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-10-20 18:21:41 -0400

    forgot JAWS

--- db/connection.js
+++ db/connection.js
@@ -3,6 +3,14 @@ const util = require ("util");
 const mysql = require ("mysql");
 
 //Connect to MySQL, don't forget to do a JAWS connection since this goes to Heroku
-const connection = mysql.createConnection ({
-    host: "localhost",
-})
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Nijm8418",
+        database: "burger_db"
+    });
+};
