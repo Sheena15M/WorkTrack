56669e72a6691a83ddad4911d76270110a1e5573
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-10-20 21:27:06 -0400

    more functions

--- index.js
+++ index.js
@@ -177,3 +177,101 @@ require("console.table");
           
             loadMainPrompts();
           }
+
+          async function removeEmployee() {
+            const employees = await db.findAllEmployees();
+          
+            const employeeChoices = employees.map(({ id, first_name, last_name }) => ({
+              name: `${first_name} ${last_name}`,
+              value: id
+            }));
+          
+            const { employeeId } = await prompt([
+              {
+                type: "list",
+                name: "employeeId",
+                message: "Which employee do you want to remove?",
+                choices: employeeChoices
+              }
+            ]);
+          
+            await db.removeEmployee(employeeId);
+          
+            console.log("Removed employee from the database");
+          
+            loadMainPrompts();
+          }
+          
+          async function updateEmployeeRole() {
+            const employees = await db.findAllEmployees();
+          
+            const employeeChoices = employees.map(({ id, first_name, last_name }) => ({
+              name: `${first_name} ${last_name}`,
+              value: id
+            }));
+          
+            const { employeeId } = await prompt([
+              {
+                type: "list",
+                name: "employeeId",
+                message: "Which employee's role do you want to update?",
+                choices: employeeChoices
+              }
+            ]);
+
+            const roles = await db.findAllRoles();
+  
+    const roleChoices = roles.map(({ id, title }) => ({
+      name: title,
+      value: id
+    }));
+  
+    const { roleId } = await prompt([
+      {
+        type: "list",
+        name: "roleId",
+        message: "Which role do you want to assign the selected employee?",
+        choices: roleChoices
+      }
+    ]);
+  
+    await db.updateEmployeeRole(employeeId, roleId);
+  
+    console.log("Updated employee's role");
+  
+    loadMainPrompts();
+  }
+  
+  async function updateEmployeeManager() {
+    const employees = await db.findAllEmployees();
+  
+    const employeeChoices = employees.map(({ id, first_name, last_name }) => ({
+      name: `${first_name} ${last_name}`,
+      value: id
+    }));
+  
+    const { employeeId } = await prompt([
+      {
+        type: "list",
+        name: "employeeId",
+        message: "Which employee's manager do you want to update?",
+        choices: employeeChoices
+      }
+    ]);
+  
+    const managers = await db.findAllPossibleManagers(employeeId);
+  
+    const managerChoices = managers.map(({ id, first_name, last_name }) => ({
+      name: `${first_name} ${last_name}`,
+      value: id
+    }));
+  
+    const { managerId } = await prompt([
+      {
+        type: "list",
+        name: "managerId",
+        message:
+          "Which employee do you want to set as manager for the selected employee?",
+        choices: managerChoices
+      }
+    ]);
