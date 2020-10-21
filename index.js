0319c18ac63ad4509951fd5c7b139834c3711250
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-10-20 20:33:01 -0400

    prompts started

--- index.js
+++ index.js
@@ -13,3 +13,28 @@ require("console.table");
   
     loadMainPrompts();
   }
+//Here are the prompts....has a similar feel to the quiz homework, except more extensive
+  async function loadMainPrompts() {
+    const { choice } = await prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "What would you like to do?",
+        choices: [
+          {
+            name: "View All Employees",
+            value: "VIEW_EMPLOYEES"
+          },
+          {
+            name: "View All Employees By Department",
+            value: "VIEW_EMPLOYEES_BY_DEPARTMENT"
+          },
+          {
+            name: "View All Employees By Manager",
+            value: "VIEW_EMPLOYEES_BY_MANAGER"
+          },
+          {
+            name: "Add Employee",
+            value: "ADD_EMPLOYEE"
+          },
+          {
