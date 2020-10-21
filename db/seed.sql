cd76d19d989ba56fa2281b38cdf9cfd7875e5ffe
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-10-20 20:21:51 -0400

    more code in seeds

--- db/seed.sql
+++ db/seed.sql
@@ -9,3 +9,5 @@ INSERT INTO role
 VALUES
 ('Development Lead', 150000, 1), ('Developer', 100000, 1),
 ('Lead Test Engineer', 150000, 2), ('Test Engineer', 100000, 2),
+('HR Manager', 110000, 3), ('HR Representive, 80000, 3'),
+('Operations Lead', 140000, 4), ('Operations Engineer', 90000, 4);
